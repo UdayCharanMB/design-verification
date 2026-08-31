8dc1c20c8195280ae5390371e79599553c4adaf0
Uday Charan <savithriuday3@gmail.com>
2026-08-31 14:56:53 +0530

    Add Verilog module for all basic logic gates

---
+++ Verilog examples/Logic Gates/all_gates/design.v
@@ -0,0 +1,19 @@
+module all_gates(input a,b,
+                 output and_out,
+                 output or_out,
+                 output not_out,
+                 output nand_out,
+                 output nor_out,
+                 output xor_out,
+                 output xnor_out);
+  assign and_out= a & b;
+  assign or_out= a | b;
+  assign not_out= ~ b;
+  assign nand_out= ~(a & b);
+  assign nor_out= ~(a | b);
+  assign xor_out= a ^ b;
+  assign xnor_out= ~(a ^ b);
+endmodule
+  
+                 
+                 
