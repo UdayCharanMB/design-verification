aa69340824e5aef6a57bba6373e741a0bc685091
Uday Charan <savithriuday3@gmail.com>
2026-08-31 14:22:49 +0530

    Add testbench for OR gate in Verilog

---
+++ Verilog examples/Logic Gates/OR gate/testbench.v
@@ -0,0 +1,15 @@
+module tb;
+  reg a,b;
+  wire y;
+  or_gate uut(.a(a),.b(b),.y(y));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,tb);
+    $monitor("a=%0d b=%0d y=%0d",a,b,y);
+    a=0;b=0;#10;
+    a=0;b=1;#10;
+    a=1;b=0;#10;
+    a=1;b=1;#10;
+    $finish;
+  end
+endmodule
