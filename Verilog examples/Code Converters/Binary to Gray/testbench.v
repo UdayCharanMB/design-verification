9ae1c4dc29ca497b3ce42036870a09c6fdb62f20
Uday Charan <savithriuday3@gmail.com>
2026-09-02 23:13:17 +0530

    Add testbench for Binary to Gray converter

---
+++ Verilog examples/Code Converters/Binary to Gray/testbench.v
@@ -0,0 +1,28 @@
+module tb_binary_to_gray;
+
+reg  [3:0] binary;
+wire [3:0] gray;
+
+binary_to_gray DUT (
+    .binary(binary),
+    .gray(gray)
+);
+
+initial begin
+
+    $monitor("Binary = %b | Gray = %b", binary, gray);
+
+    binary = 4'b0000;
+    #10 binary = 4'b0001;
+    #10 binary = 4'b0010;
+    #10 binary = 4'b0011;
+    #10 binary = 4'b0100;
+    #10 binary = 4'b0101;
+    #10 binary = 4'b0110;
+    #10 binary = 4'b0111;
+    #10 binary = 4'b1000;
+    #10 $finish;
+
+end
+
+endmodule
