1f4059252f26a4bd912b19c0f43b9865fd637bf5
Uday Charan <savithriuday3@gmail.com>
2026-09-02 23:40:00 +0530

    Add testbench for seven segment decoder

---
+++ Verilog examples/Code Converters/Seven Segment Decoder/testbench.v
@@ -0,0 +1,29 @@
+module tb_seven_segment;
+
+reg [3:0] bin;
+wire [6:0] seg;
+
+seven_segment DUT (
+    .bin(bin),
+    .seg(seg)
+);
+
+initial begin
+
+    $monitor("bin=%b | seg=%b", bin, seg);
+
+    bin = 4'b0000;
+    #10 bin = 4'b0001;
+    #10 bin = 4'b0010;
+    #10 bin = 4'b0011;
+    #10 bin = 4'b0100;
+    #10 bin = 4'b0101;
+    #10 bin = 4'b0110;
+    #10 bin = 4'b0111;
+    #10 bin = 4'b1000;
+    #10 bin = 4'b1001;
+
+    #10 $finish;
+end
+
+endmodule
