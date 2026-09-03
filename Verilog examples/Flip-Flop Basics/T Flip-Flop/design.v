a9ccc985777e0199649b33ca7f9aba700aadb696
Uday Charan <savithriuday3@gmail.com>
2026-09-03 10:59:46 +0530

    Add T Flip-Flop module implementation

---
+++ Verilog examples/Flip-Flop Basics/T Flip-Flop/design.v
@@ -0,0 +1,18 @@
+module t_ff (
+    input T,
+    input clk,
+    input reset,
+    output reg Q
+);
+
+always @(posedge clk) begin
+    if (reset)
+        Q <= 0;
+    else if (T)
+        Q <= ~Q;
+    else
+        Q <= Q;
+end
+
+endmodule
+
