889c158016f81a483bb48431ea696ca39ec69bc3
Uday Charan <savithriuday3@gmail.com>
2026-09-04 10:26:50 +0530

    Add SIPO shift register module in design.v

---
+++ Verilog examples/Shift Registers/SIPO Shift Register/design.v
@@ -0,0 +1,15 @@
+module sipo (
+    input clk,
+    input reset,
+    input serial_in,
+    output reg [3:0] parallel_out
+);
+
+always @(posedge clk) begin
+    if (reset)
+        parallel_out <= 4'b0000;
+    else
+        parallel_out <= {parallel_out[2:0], serial_in};
+end
+
+endmodule
